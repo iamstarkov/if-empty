9f488102e903037b8ee71fecac8d07d69b2e94e7
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-25 15:21:37 +0100

    implement thing

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-export default function ifEmpty(input) {
-  return input;
-};
+import R from 'ramda';
+
+// ifEmpty :: String -> String -> true | String
+const ifEmpty = R.uncurryN(2, R.pipe(
+  R.always,
+  R.ifElse(
+    R.anyPass([R.isEmpty, R.isNil]),
+    R.__,
+    R.T)
+));
+
+export default ifEmpty;
