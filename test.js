24cf38b3fcc70a9f0661b39b6f6a26f0ff2bf4e8
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-25 14:55:54 +0100

    write tests

--- test.js
+++ test.js
@@ -2,8 +2,13 @@
 import { equal } from 'assert';
 import ifEmpty from './index';
 
-it('should ifEmpty', () =>
-  equal(ifEmpty('unicorns'), 'unicorns'));
+it('should ifEmpty', () => {
+  equal(ifEmpty('unicorns'), 'unicorns');
+  equal(ifEmpty('unicorns', null), 'unicorns');
+  equal(ifEmpty('unicorns')(), 'unicorns');
+  equal(ifEmpty('unicorns', 'not-empty'), true);
+  equal(ifEmpty('unicorns')('not-empty'), true);
+});
 
 it('should ifEmpty invalid input', () =>
   equal(ifEmpty(), undefined));
